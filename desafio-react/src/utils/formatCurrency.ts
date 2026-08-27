29bae63830ab31f8955051894c6378ff4adc51cf
JLineBRSilva <j.silvabroccolo@gmail.com>
2026-08-26 22:03:58 -0300

    refactor: altera função p/ somente receber o param. do valor a ser formatado

--- desafio-react/src/utils/formatCurrency.ts
+++ desafio-react/src/utils/formatCurrency.ts
@@ -1,9 +1,9 @@
 interface currencyProps {
     value: number | string
-    currency: 'BRL' | string
+    currency?: 'BRL' | string
 }
 
 
-export function formatCurrencyForBRL({ value, currency }: currencyProps) {
-    return value.toLocaleString('pt-BR', { style: 'currency', currency })
+export function formatCurrencyForBRL({ value }: currencyProps) {
+    return value.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
 }
