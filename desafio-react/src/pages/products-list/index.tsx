5c0b98a61a2eca7bdf4cd4ffa0dd1465d390b9ee
JLineBRSilva <j.silvabroccolo@gmail.com>
2026-08-26 21:59:40 -0300

    refactor: altera elementos de construção da tabela

--- desafio-react/src/pages/products-list/index.tsx
+++ desafio-react/src/pages/products-list/index.tsx
@@ -2,24 +2,18 @@ import Paper from '@mui/material/Paper';
 import { styled } from '@mui/material/styles';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
-import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import { useEffect, useState } from 'react';
+import LayoutPage from '../../layouts';
 import { getAllProducts } from '../../services/products.service';
 import type { ProductsProps } from '../../types/product.types';
+import { checkProductStatus } from '../../utils/checkItemActive';
+import { formatCurrencyForBRL } from '../../utils/formatCurrency';
+import styles from './productsList.module.css';
 
-
-const StyledTableCell = styled(TableCell)(({ theme }) => ({
-  [`&.${tableCellClasses.head}`]: {
-    backgroundColor: theme.palette.common.black,
-    color: theme.palette.common.white,
-  },
-  [`&.${tableCellClasses.body}`]: {
-    fontSize: 14,
-  },
-}));
 
 const StyledTableRow = styled(TableRow)(({ theme }) => ({
   '&:nth-of-type(odd)': {
@@ -32,11 +26,8 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
 }));
 
 
-
 export default function ProductsList() {
-
   const [fillDb, setFillDb] = useState<ProductsProps[]>([])
-
 
   async function fillDatabase() {
     let loadData = await getAllProducts()
@@ -44,41 +35,46 @@ export default function ProductsList() {
     return fillDb
   }
 
-
   useEffect(() => {
     fillDatabase()
   }, [])
 
 
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 700 }} aria-label="customized table">
-        <TableHead>
-          <TableRow>
-            <StyledTableCell>ID</StyledTableCell>
-            <StyledTableCell>Nome</StyledTableCell>
-            <StyledTableCell>Categoria</StyledTableCell>
-            <StyledTableCell>Preço</StyledTableCell>
-            <StyledTableCell>Estoque</StyledTableCell>
-            <StyledTableCell>Ativo</StyledTableCell>
-          </TableRow>
-        </TableHead>
+    <LayoutPage titlePage='Produtos'
+      subtitle='Gerencie seu inventário e catálogo de produtos'
+    >
 
-        <TableBody>
-          {fillDb.map((row: ProductsProps) => (
-            <StyledTableRow key={row.nome}>
-              <StyledTableCell component="th" scope="row">
-                {row.id}
-              </StyledTableCell>
-              <StyledTableCell>{row.nome}</StyledTableCell>
-              <StyledTableCell>{row.categoria}</StyledTableCell>
-              <StyledTableCell>{row.preco}</StyledTableCell>
-              <StyledTableCell>{row.estoque}</StyledTableCell>
-              <StyledTableCell>{row.ativo}</StyledTableCell>
-            </StyledTableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+      <TableContainer component={Paper}>
+        <Table aria-label="customized table">
+          <TableHead>
+            <TableRow >
+              <TableCell className={styles.tableHeader}>ID</TableCell>
+              <TableCell className={styles.tableHeader}>Nome</TableCell>
+              <TableCell className={styles.tableHeader}>Categoria</TableCell>
+              <TableCell className={styles.tableHeader}>Preço</TableCell>
+              <TableCell className={styles.tableHeader}>Estoque</TableCell>
+              <TableCell className={styles.tableHeader}>Ativo</TableCell>
+            </TableRow>
+          </TableHead>
+
+          <TableBody>
+            {fillDb.map((row: ProductsProps, index) => (
+              <StyledTableRow key={row.id}>
+                <TableCell key={index} component="th" scope="row">
+                  {row.id}
+                </TableCell>
+                <TableCell>{row.nome}</TableCell>
+                <TableCell>{row.categoria}</TableCell>
+                <TableCell>{formatCurrencyForBRL({ value: row.preco })}</TableCell>
+                <TableCell>{row.estoque}</TableCell>
+                <TableCell>{checkProductStatus(row.ativo)}</TableCell>
+              </StyledTableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+
+    </LayoutPage>
   );
 }
